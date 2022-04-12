f5343d8ad9786c3e907af2d81b8fced1da782c39
YDFlame13 <ydflame@tom.com>
2022-04-12 09:57:01 +0800

    一个空的front分支

--- user_management_back/src/main/java/com/user_management/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.user_management.utils;
-
-public class Constant {
-    public static Long EXPIREMILLS= Long.valueOf(1000*60*60*24*3);
-    public static int PAGELIMIT=12;
-    public static String KEYWORD="webjschz";
-    public static String HEADERNAME="webjschzToken";
-    public static String[] EXCLUDEPATHS = new String[]{"/login", "/register"};
-}
